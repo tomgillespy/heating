c5f750e2ebf3f8266a2bc1c3142457ac199ae17b
Tom Gillespy <thomasgillespy@gmail.com>
2016-12-04 01:34:19 +0000

    Added some shit. Doesn't look very good, and the fonts go on device, but functionality is somewhat coming on.

---
+++ HeatingController/www/scripts/app.js
@@ -0,0 +1,21 @@
+﻿
+function AppViewModel() {
+    var self = this;
+    self.currentTemp = ko.observable(16);
+    self.boilerOn = ko.observable(true);
+    self.nextTime = ko.observable('23:59');
+    self.nextTarget = ko.observable(17);
+    self.targetTemp = ko.observable(18);
+    self.targetSince = ko.observable('18:00');
+
+    self.heatingiconsrc = ko.computed(function () {
+        if (self.boilerOn()) {
+            return 'images/heating-on.png';
+        } else {
+            return 'images/heating-off.png';
+        }
+    });
+    self.targetTemp = ko.observable(18);
+}
+var vm = new AppViewModel();
+ko.applyBindings(vm);
--- HeatingController/www/scripts/index.js
+++ HeatingController/www/scripts/index.js
@@ -11,11 +11,7 @@
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var element = document.getElementById("deviceready");
-        element.innerHTML = 'Device Ready';
-        element.className += ' ready';
+
     };
 
     function onPause() {
